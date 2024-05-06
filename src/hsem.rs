68defd79b1a1d79efc6c661aeb9b38723b72426f
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2024-05-06 11:11:19 +0900

    HSEM register access method is changed.

--- src/hsem.rs
+++ src/hsem.rs
@@ -1,12 +1,10 @@
 //! Hardware Semaphore (HSEM)
 
-use core::marker::PhantomData;
-
 use crate::stm32;
 use crate::rcc::{rec, ResetEnable};
 
-const CPUID_CPU1:u8 = 0x01;
-const CPUID_CPU2:u8 = 0x03;
+const CPUID_CPU1:u8 = 0x03;
+const CPUID_CPU2:u8 = 0x01;
 
 #[cfg(feature="cm7")]
 const CPUID:u8 = CPUID_CPU1;
@@ -17,11 +15,10 @@ pub trait HsemExt {
     type Rec: ResetEnable;
 
     fn hsem(self, prec: Self::Rec) -> Hsem;
-    fn hsem_without_reset(self) -> Hsem;
+    fn hsem_without_reset(self, prec: Self::Rec) -> Hsem;
 }
 
 pub struct Hsem {
-    pub(crate) rb: stm32::HSEM,
 }
 
 impl HsemExt for stm32::HSEM {
@@ -29,43 +26,86 @@ impl HsemExt for stm32::HSEM {
     fn hsem(self, prec: Self::Rec) -> Hsem {
         prec.enable().reset();
         Hsem {
-            rb: self
         }
     }
-    fn hsem_without_reset(self) -> Hsem {
+    fn hsem_without_reset(self, prec: Self::Rec) -> Hsem {
+        prec.enable();
         Hsem {
-            rb: self
         }
     }
 }
 
 impl Hsem {
-    pub fn sema<'a>(&'a self, no:usize) -> Sema<'a> {
+    pub fn sema(&self, no:usize) -> Sema {
         Sema  {
             no,
-            rb: &self.rb
         }
     }
 }
 
-pub struct Sema<'a> {
+pub struct Sema {
     no: usize,
-    rb: &'a stm32::HSEM
 }
 
-impl<'a> Sema<'a> {
-    pub fn enable_interrupt(&mut self) {
+impl Sema {
+
+    pub fn enable_irq(&mut self) {
+        let rb_ptr = crate::pac::HSEM::ptr();
         match CPUID {
-            CPUID_CPU1 => { // cm7
-                self.rb.c1ier().modify(|r,w| unsafe { w.bits(r.bits() | 0b1 << self.no) });
+            CPUID_CPU1 => unsafe { // cm7
+                (*rb_ptr).c1ier().modify(|r,w| { w.bits(r.bits() | 0b1 << self.no) });
             },
-            CPUID_CPU2 => { // cm4
-                self.rb.c2ier().modify(|r,w| unsafe { w.bits(r.bits() | 0b1 << self.no) });
+            CPUID_CPU2 => unsafe { // cm4
+                (*rb_ptr).c2ier().modify(|r,w| { w.bits(r.bits() | 0b1 << self.no) });
             },
             _ => unreachable!()
         }
-        self.rb.r(self.no).modify(|r,w| unsafe { w.bits(
-            r.bits() | (0b1 << self.no)
-        )})
+    }
+
+    pub fn disable_irq(&mut self) {
+        let rb_ptr = crate::pac::HSEM::ptr();
+        let ch_bit : u32 = 0b1 << self.no;
+        match CPUID {
+            CPUID_CPU1 => unsafe { // cm7
+                (*rb_ptr).c1ier().modify(|r,w| { w.bits(r.bits() & !ch_bit) });
+            },
+            CPUID_CPU2 => unsafe { // cm4
+                (*rb_ptr).c2ier().modify(|r,w| { w.bits(r.bits() & !ch_bit) });
+            },
+            _ => unreachable!()
+        }
+    }
+
+    pub fn clear_irq(&mut self){
+        let rb_ptr = crate::pac::HSEM::ptr();
+        let ch_bit : u32 = 0b1 << self.no;
+        match CPUID {
+            CPUID_CPU1 => unsafe { // cm7
+                (*rb_ptr).c1icr().write(|w| { w.bits(ch_bit) });
+            },
+            CPUID_CPU2 => unsafe { // cm4
+                (*rb_ptr).c2icr().write(|w| { w.bits(ch_bit) });
+            },
+            _ => unreachable!()
+        }
+    }
+
+    pub fn fast_take(&mut self) -> bool {
+        let rb_ptr = crate::pac::HSEM::ptr();
+        let rlr = unsafe {
+            let rlr = (*rb_ptr).rlr(self.no).read().bits();
+            ((rlr & (0x1 << 31)) == (0x1 << 31), ((rlr >> 8) & 0x0000_000F), (rlr & 0x000000FF))
+        };
+        #[cfg(feature = "log")]
+        log::debug!("fast_take {:?}", rlr);
+        rlr.0 && (rlr.1 == CPUID.into()) && rlr.2 == 0
+    }
+
+    pub fn release(&mut self, proc_id: u8) {
+        let rb_ptr = crate::pac::HSEM::ptr();
+        let r:u32 = ((CPUID as u32) << 8) | (proc_id as u32);
+        #[cfg(feature = "log")]
+        log::debug!("release 0x{:08x}", r);
+        unsafe { (*rb_ptr).r(self.no).modify(|_,w| { w.bits(r) }) };
     }
 }
